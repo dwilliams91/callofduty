bbd4985ce8ab218ed274721c88578caa7ff071f1
Your Github User Name <david.lav.williams@gmail.com>
2021-12-10 13:24:53 -0600

    its applying the attachments incorrectly

--- callofdutyapi/views/createdclass.py
+++ callofdutyapi/views/createdclass.py
@@ -18,24 +18,47 @@ class CreatedClasses(ViewSet):
             created_class_selected, many=False, context={'request':request}
         )
         class_workable_data=serializer.data
-        print(class_workable_data)
-        print(class_workable_data['base_gun'])
+        # print(class_workable_data)
+        # print(class_workable_data['base_gun'])
         my_gun_stats=GunStat.objects.filter(gun_id=class_workable_data['base_gun'])
         my_gun_stats_workable=GunStatSerlializer(my_gun_stats, many=True, context={'request':request})
         my_gun_stats_workable=my_gun_stats_workable.data
         
-        print(my_gun_stats_workable[0]['stat_id']['name'])
+        # print(my_gun_stats_workable[0]['stat_id']['name'])
         new_object={}
         for item in my_gun_stats_workable:
             new_object[item['stat_id']['name']]=item['value']
-        print(new_object)
+        # print(new_object)
         
         # getting attachments
         my_slots=AttachmentSlot.objects.filter(createdclass_id=pk)
         my_slots_workable=AttachmentSlotSerializer(my_slots, many=True, context={'request':request}).data
-        for item in my_slots_workable:
-            attachment_id=my_slots_workable[0]['attachment_id']
-            print(attachment_id)
+        # print(my_slots_workable)
+        list_of_attachments=[]
+        for x in range(len(my_slots_workable)):
+            attachment_id=my_slots_workable[x]['attachment_id']
+            list_of_attachments.append(attachment_id)
+        # print(list_of_attachments)
+        all_attachment_stats_weird=[]
+        for item in list_of_attachments:
+            attachment_stat=AttachmentStat.objects.filter(attachment_id=item)
+            attachment_stat_workable=AttachmentStatSerializer(attachment_stat, many=True, context={'request':request}).data
+            all_attachment_stats_weird.append(attachment_stat_workable)
+
+        all_attachment_stats=[]
+        for item in all_attachment_stats_weird:
+            for stat in item:
+                all_attachment_stats.append(stat)
+        # print(all_attachment_stats[0])
+        attachment_object={}
+        for item in all_attachment_stats:
+            if item['stat_id']['name'] not in attachment_object.keys():
+                attachment_object[item['stat_id']['name']]=float(item['effect'])
+            else:
+                new_value=attachment_object[item['stat_id']['name']]+float(item['effect'])
+                attachment_object[item['stat_id']['name']]=new_value
+        
+        # for item in all_attachment_stats: 
         # get createdclass
         # get associated gun
         # get associated gun stats
@@ -43,6 +66,14 @@ class CreatedClasses(ViewSet):
         # get associated attachment
         # get associated attachment stats
         # modify value for gunstat
+        print(new_object)
+        for attachment_stat in attachment_object.keys():
+            for gun_stat in new_object.keys():
+                if attachment_stat==gun_stat:
+                    new_object[gun_stat]=float(new_object[gun_stat])*attachment_object[attachment_stat]
+        print("dude")
+        print(new_object)
+        
         return Response(serializer.data)
 
 class GunSerlializer(serializers.ModelSerializer):
@@ -80,6 +111,7 @@ class AttachmentSerializer(serializers.ModelSerializer):
         fields=['name']
 
 class AttachmentStatSerializer(serializers.ModelSerializer):
+    stat_id=StatSerlializer(many=False)
     class Meta:
-        model=AttachmentSlot
+        model=AttachmentStat
         fields=['attachment_id','stat_id', 'effect']
