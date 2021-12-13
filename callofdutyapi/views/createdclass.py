a9def24d7e0a1abc2ae5d9d7df609d8bdb026c9c
Your Github User Name <david.lav.williams@gmail.com>
2021-12-13 09:38:12 -0600

    retrieving a class gets the modified stats

--- callofdutyapi/views/createdclass.py
+++ callofdutyapi/views/createdclass.py
@@ -13,43 +13,41 @@ from callofdutyapi.models import Attachment
 
 class CreatedClasses(ViewSet):
     def retrieve(self, request, pk=None):
+        # get the selected class
         created_class_selected=CreatedClass.objects.get(pk=pk)
-        serializer=CreatedClassSerlializer(
+        selected_class_serializer=CreatedClassSerlializer(
             created_class_selected, many=False, context={'request':request}
         )
-        class_workable_data=serializer.data
-        # print(class_workable_data)
-        # print(class_workable_data['base_gun'])
+        class_workable_data=selected_class_serializer.data
+        
+        # get the associated gunstats 
         my_gun_stats=GunStat.objects.filter(gun_id=class_workable_data['base_gun'])
         my_gun_stats_workable=GunStatSerlializer(my_gun_stats, many=True, context={'request':request})
         my_gun_stats_workable=my_gun_stats_workable.data
         
-        # print(my_gun_stats_workable[0]['stat_id']['name'])
-        new_object={}
+        # create a new dictionary with the keys and values of the associated gunstats
+        selected_gun_with_stats={}
         for item in my_gun_stats_workable:
-            new_object[item['stat_id']['name']]=item['value']
-        # print(new_object)
+            selected_gun_with_stats[item['stat_id']['name']]=item['value']
         
         # getting attachments
         my_slots=AttachmentSlot.objects.filter(createdclass_id=pk)
         my_slots_workable=AttachmentSlotSerializer(my_slots, many=True, context={'request':request}).data
-        # print(my_slots_workable)
         list_of_attachments=[]
         for x in range(len(my_slots_workable)):
             attachment_id=my_slots_workable[x]['attachment_id']
             list_of_attachments.append(attachment_id)
-        # print(list_of_attachments)
-        all_attachment_stats_weird=[]
+        all_attachment_stats_out_of_order=[]
         for item in list_of_attachments:
             attachment_stat=AttachmentStat.objects.filter(attachment_id=item)
             attachment_stat_workable=AttachmentStatSerializer(attachment_stat, many=True, context={'request':request}).data
-            all_attachment_stats_weird.append(attachment_stat_workable)
-
+            all_attachment_stats_out_of_order.append(attachment_stat_workable)
         all_attachment_stats=[]
-        for item in all_attachment_stats_weird:
+        for item in all_attachment_stats_out_of_order:
             for stat in item:
                 all_attachment_stats.append(stat)
-        # print(all_attachment_stats[0])
+                
+        # create a dictionary for attachments
         attachment_object={}
         for item in all_attachment_stats:
             if item['stat_id']['name'] not in attachment_object.keys():
@@ -58,23 +56,13 @@ class CreatedClasses(ViewSet):
                 new_value=attachment_object[item['stat_id']['name']]+float(item['effect'])
                 attachment_object[item['stat_id']['name']]=new_value
         
-        # for item in all_attachment_stats: 
-        # get createdclass
-        # get associated gun
-        # get associated gun stats
-        # get associated attachmentslots
-        # get associated attachment
-        # get associated attachment stats
-        # modify value for gunstat
-        print(new_object)
+        # modify the selected guns properly
         for attachment_stat in attachment_object.keys():
-            for gun_stat in new_object.keys():
+            for gun_stat in selected_gun_with_stats.keys():
                 if attachment_stat==gun_stat:
-                    new_object[gun_stat]=float(new_object[gun_stat])*attachment_object[attachment_stat]
-        print("dude")
-        print(new_object)
-        
-        return Response(serializer.data)
+                    selected_gun_with_stats[gun_stat]=float(selected_gun_with_stats[gun_stat])*attachment_object[attachment_stat]+float(selected_gun_with_stats[gun_stat])
+        selected_gun_with_stats['gun']=class_workable_data['base_gun']
+        return Response(selected_gun_with_stats)
 
 class GunSerlializer(serializers.ModelSerializer):
     class Meta:
